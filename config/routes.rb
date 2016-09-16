d2973d0b63deca4da30dd27a16eba1ffdc8de974
ddavisgraphics <dave@ddavisgraphics.com>
2016-09-16 13:08:13 -0400

    Creating new Routes  and forcing admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :libraries, path: '/admin/libraries'
-  resources :departments, path: '/admin/departments'
+
+  # forces the controllers to use the admin name space
+  # this is going to allow for the addition of a function to restrict access
+  # resources generates all routes for crud of libraries, departments, users, etc.
+  scope '/admin' do
+    resources :libraries, :departments, :users, module: 'admin'
+  end
+
+  # admin
+  get     '/admin', to: 'admin#index'
+
+  # login
+  get     '/login',   to: 'sesions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
+
+  # root
+  root 'public#index'
+
 end
