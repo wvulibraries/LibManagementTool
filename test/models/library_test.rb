0239b5195165c5c6369aa2f16c86b9e62626e1bb
ddavisgraphics <dave@ddavisgraphics.com>
2016-09-19 15:57:58 -0400

    Test model to not allow a library to save without a name

--- test/models/library_test.rb
+++ test/models/library_test.rb
@@ -4,4 +4,9 @@ class LibraryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "can not save library without a name" do
+    library = Library.new
+    assert_not = library.save, "Saved without a name"
+  end 
 end
