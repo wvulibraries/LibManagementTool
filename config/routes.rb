9d7da4d702483d4cb931d7739e808be1e4a12805
ddavisgraphics <dave@ddavisgraphics.com>
2016-09-19 15:59:30 -0400

    move admin route for index to make test pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
+  # admin
+  get     '/admin', to: 'admin#index'
+
   # forces the controllers to use the admin name space
   # this is going to allow for the addition of a function to restrict access
   # resources generates all routes for crud of libraries, departments, users, etc.
+
   scope '/admin' do
     resources :libraries, :departments, :users, module: 'admin'
   end
-
-  # admin
-  get     '/admin', to: 'admin#index'
 
   # login
   get     '/login',   to: 'sesions#new'
