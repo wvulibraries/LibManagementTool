e2112fddad34dd50fa8bea5bf731fd21a8da5cd2
ddavisgraphics <dave@ddavisgraphics.com>
2016-09-23 10:48:39 -0400

    remove un-needed departments function

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,7 +1,4 @@
 class Library < ApplicationRecord
   has_many :departments, dependent: :destroy
-
-  def departments
-      departments = Department.where(:library_id => self.id)
-  end
+  has_many :normal_hours, as: :resource
 end
