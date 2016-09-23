fb7787a9512b06ca8fea0d6f13cb09addce268ce
ddavisgraphics <dave@ddavisgraphics.com>
2016-09-23 10:47:55 -0400

    formats name so user can tell where department is located.

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,8 @@
 class Department < ApplicationRecord
   belongs_to :library, inverse_of: :departments
+  has_many :normal_hours, as: :resource
+
+  def reformat_name
+    return self.name + " - " + self.library.name
+  end
 end
