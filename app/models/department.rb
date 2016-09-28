9b365d6109a39a5056a361072cbae0f85070dba2
ddavisgraphics <dave@ddavisgraphics.com>
2016-09-28 15:45:58 -0400

    Many to Many Links and Formatting of JSON

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,7 @@
 class Department < ApplicationRecord
   belongs_to :library, inverse_of: :departments
   has_many :normal_hours, as: :resource
+  has_many :special_hours, as: :special
 
   def reformat_name
     return self.name + " - " + self.library.name
--- app/models/library.rb
+++ app/models/library.rb
@@ -1,4 +1,5 @@
 class Library < ApplicationRecord
   has_many :departments, dependent: :destroy
   has_many :normal_hours, as: :resource
+  has_many :special_hours, as: :special
 end
