0c74e0f1463a846124b9a95acfe7b9b02e96048f
ddavisgraphics <dave@ddavisgraphics.com>
2016-10-25 14:36:28 -0400

    Adding validations for unit tests

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,6 +3,12 @@ class Department < ApplicationRecord
   has_many :normal_hours, as: :resource
   has_many :special_hours, as: :special
 
+  validates :name, presence: true
+  validates :id,  numericality: { only_integer: true, allow_nil: true }
+  validates :library_id,  numericality: { only_integer: true, allow_nil: false, message: "%{value} must not be nil and must be an valid id." }
+  validates :description, length: { maximum: 500, message: "%{value} is too long, please shorten the description to 500 characters." }, allow_blank: true
+
+
   def reformat_name
     return self.name + " - " + self.library.name
   end
--- app/models/library.rb
+++ app/models/library.rb
@@ -2,4 +2,8 @@ class Library < ApplicationRecord
   has_many :departments, dependent: :destroy
   has_many :normal_hours, as: :resource
   has_many :special_hours, as: :special
+
+  validates :name, presence: true
+  validates :id,  numericality: { only_integer: true, allow_nil: true }
+  validates :description, length: { maximum: 500, message: "%{value} is too long, please shorten the description to 500 characters." }, allow_blank: true
 end
