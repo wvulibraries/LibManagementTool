9ddbb2afddf02bdfd699a00beaa4c78587979285
ddavisgraphics <dave@ddavisgraphics.com>
2016-10-25 14:36:41 -0400

    removing unessary commentary

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,12 @@
 require_relative 'boot'
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module LibraryManagementTool
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
+    ## Set Configuration of Time Zone Here
+    ## Although this should be handled by the server
     config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.default_timezone = :local
   end
