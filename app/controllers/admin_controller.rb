ebc41331882a265a474850c6369c49b469fda814
ddavisgraphics <dave@ddavisgraphics.com>
2016-11-10 18:15:56 -0500

    Working with adding users and modifying auth

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,14 @@
+require "rack-cas-rails"
 class AdminController < ApplicationController
-  # before_action :require_admin
-  #
-  # private
-  #   def require_admin
-  #     unless current_user.admin?
-  #       redirect_to root_path
-  #     end
-  #   end
+  before_action :authenticate!
+  before_action :check_secret_CAS
+
+  private
+  def check_secret_CAS
+    secret = Digest::MD5.hexdigest("Library")
+    if secret != session['cas']['secret']
+      session.delete('cas')
+      redirect_to root_path, notice: 'You have failed this Application with a faulty login!'
+    end
+  end
 end
