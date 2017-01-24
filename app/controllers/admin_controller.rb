507b072472d3687d9318cfa032ae6aaa766ecec5
ddavisgraphics <dave@ddavisgraphics.com>
2017-01-24 14:21:53 -0500

    Removing old CAS and implementing new cas filters.  Working on testing.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,25 @@
-require "rack-cas-rails"
 class AdminController < ApplicationController
-  before_action :authenticate!
-  before_action :check_secret_CAS
+  # perform filter before action
+  before_action CASClient::Frameworks::Rails::Filter
+  before_action :check_permissions
+
+  def index
+    @username = session[:cas_user]
+    @email = session[:cas_extra_attributes][:mail]
+    @last_name = session[:cas_extra_attributes][:sn]
+    flash.now[:success] = "Welcome #{@username}!  You have been sucessfully logged in!"
+  end
 
   private
-  def check_secret_CAS
-    secret = Digest::MD5.hexdigest("Library")
-    if secret != session['cas']['secret']
-      session.delete('cas')
-      redirect_to root_path, notice: 'You have failed this Application with a faulty login!'
+  def check_permissions
+    has_permission = User.where(username: session[:cas_user]).exists?
+    if !has_permission
+      redirect_to root_path, error: 'You do not have administrative permissions, please contact the system administrator if you feel that this has been reached in error.'
+    elsif session[:cas_user].nil? && session[:cas_last_valid_ticket]
+      redirect_to root_path, error: 'Something went wrong or a faulty login was detected.'
+    else
+      return true
     end
   end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,18 @@
 # Load the Rails application.
 require_relative 'application'
 
+# ruby cas client requirements
+require 'casclient'
+require 'casclient/frameworks/rails/filter'
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# CAS Configurations
+# See github repo for more doucmentation https://github.com/ddavisgraphics/rubycas-client.git
+CASClient::Frameworks::Rails::Filter.configure(
+  :cas_base_url => 'https://ssodev.wvu.edu/cas',
+  :login_url     => "https://ssodev.wvu.edu/cas/login",
+  :logout_url    => "https://ssodev.wvu.edu/cas/logout",
+  :validate_url  => "https://ssodev.wvu.edu/cas/proxyValidate"
+)
