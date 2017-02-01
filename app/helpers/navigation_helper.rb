2729131f936b553fc191a0d79776bf025676643a
ddavisgraphics <dave@ddavisgraphics.com>
2017-02-01 13:48:09 -0500

    Navgation building.

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,16 +1,36 @@
 module NavigationHelper
   def admin_links
-    items = [libraries_link, departments_link]
+    items = [home_link, admin_home_link, libraries_link, departments_link, users_link, normal_hours_link, special_hours_link]
     content_tag :ul, :class => 'menu' do
        items.collect {|item| concat(content_tag(:li, item))}
     end
   end
 
-  def libraries_link(html_options = {})
+  def home_link
+    link_to 'Home', root_path
+  end
+
+  def admin_home_link
+    link_to 'Admin', admin_path
+  end
+
+  def libraries_link
     link_to 'Libraries', libraries_path
   end
 
-  def departments_link(html_options = {})
+  def departments_link
     link_to 'Departments', departments_path
+  end
+
+  def users_link
+    link_to 'Users', users_path
+  end
+
+  def normal_hours_link
+    link_to 'Normal Hours', normal_hours_path
+  end
+
+  def special_hours_link
+    link_to 'Special Hours', special_hours_path
   end
 end
