47f3742fc34ac520ade965b2be86e1388967b5f6
ddavisgraphics <dave@ddavisgraphics.com>
2017-02-01 13:01:35 -0500

    User tests
    Users models, removed stuff for permissions and working towards integration tests.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user should have the necessary required validators" do
+    user = User.new
+    assert_not user.valid?
+    assert_equal [:username, :firstname, :lastname], post.errors.keys
+  end
+
+  test "user is valid" do
+    user = User.new(username:'user1', firstname: 'frank', lastname:'boyd', admin: false)
+    assert user.valid?
+  end
+
+  test "user admin key is not allowed to be null" do
+    user = User.new(username:'user1', firstname: 'frank', lastname:'boyd', admin:nil)
+    assert_not user.valid?
+  end 
 end
