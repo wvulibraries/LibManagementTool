4978fd4f4076acdef5214beeb74638df44d22932
ddavisgraphics <dave@ddavisgraphics.com>
2017-02-02 15:28:11 -0500

    Generate Defaults for working with user permissions

---
+++ app/models/user_permission.rb
@@ -0,0 +1,2 @@
+class UserPermission < ApplicationRecord
+end
---
+++ db/migrate/20170202201452_create_user_permissions.rb
@@ -0,0 +1,11 @@
+class CreateUserPermissions < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :user_permissions
+    create_table :user_permissions do |t|
+      t.string :building_type
+      t.integer :building_id, :limit => 2
+      t.string :username
+      t.timestamps
+    end
+  end
+end
