2cc74d524536cbf6b80afae8e260af36ab65fe64
ddavisgraphics <dave@ddavisgraphics.com>
2017-02-02 12:02:05 -0500

    Added Script for automatic log rotation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.log_formatter = ::Logger::Formatter.new
+  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 1, 50 * 1024 * 1024)
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
