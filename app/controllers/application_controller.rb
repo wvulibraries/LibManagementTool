d8764d7baeecbd07bafd4e509880ad5c71d15490
ddavisgraphics <dave@ddavisgraphics.com>
2017-02-09 15:36:35 -0500

    Helper to clean blank items from an array.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :warning, :danger, :info, :error
+
+  def clean_array (array_to_clean)
+    array_to_clean.reject { |item| item.blank? }
+  end
 end
