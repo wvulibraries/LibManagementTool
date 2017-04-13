0e1f2b8ee284ab5708ab6c02140e593cacb6fd45
ddavisgraphics <dave@ddavisgraphics.com>
2017-04-13 14:18:31 -0400

    Rubo Cop has been added to each of these, there is still more work to do.  Need to talk with tracy on some of these changes.

--- app/services/check_access.rb
+++ app/services/check_access.rb
@@ -1,11 +1,15 @@
 # app/services/check_access.rb
+# @author David J. Davis
+# @author Tracy A. McCormick 
+# gets departments libraries and preforms access level checks 
 class CheckAccess
-
+  # @todo 
+  # all of this can be re-written using attr_accessors 
+  # att_accessors replace the get and set methods
+  # start todo 
   def initialize (user_depts, user_libs)
-    # puts user_depts.inspect
-    # puts user_libs.inspect
-    self.set_depts(user_depts)
-    self.set_libs(user_libs)
+    set_depts(user_depts)
+    set_libs(user_libs)
   end
 
   def set_depts(depts)
@@ -23,6 +27,7 @@ class CheckAccess
   def get_libs
     @user_libs
   end
+  #end todo 
 
   def check(type, resource_id)
     @type = type.to_s.downcase
@@ -31,21 +36,22 @@ class CheckAccess
   end
 
   private
-    def check_type_access
-      if (@type === 'library')
-        user_libraries
-      elsif (@type === 'department')
-        user_departments
-      else
-        false
-      end
-    end
 
-    def user_departments
-      @user_depts.include? @resource_id
+  def check_type_access
+    if @type == 'library'
+      user_libraries
+    elsif @type == 'department'
+      user_departments
+    else
+      false
     end
+  end
 
-    def user_libraries
-      @user_libs.include? @resource_id
-    end
+  def user_departments
+    @user_depts.include? @resource_id
+  end
+
+  def user_libraries
+    @user_libs.include? @resource_id
+  end
 end
