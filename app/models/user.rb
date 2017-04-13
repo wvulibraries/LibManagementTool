0e4ab2d0d597f7090b102442b6a75939972df632
ddavisgraphics <dave@ddavisgraphics.com>
2017-04-13 14:17:58 -0400

    Rubo Cop running

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
+# User
+# @author David J. Davis
+# Allows users to be added to the system.
+# This only works for CAS users at the current time
 class User < ApplicationRecord
   validates_presence_of :username, :firstname, :lastname
-  validates :admin, inclusion: { in: [true, false] } # makes sure that null is not an option
+  validates :admin, inclusion: { in: [true, false] }
   has_one :user_permissions
 end
